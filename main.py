4a6bb467827f292bae68e5a82806ab1cf4142eb6
Robert Eanes <95715277+rheanes@users.noreply.github.com>
2022-08-06 00:20:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def get_driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument("disable-infobars")
+    options.add_argument("start-maximized")
+    options.add_argument("disable-dev-shm-usage")
+    options.add_argument("no-sandbox")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_argument("disable-blink-features=AutomationControlled")
+
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://members.cyclebar.com/auth/login?location_id=cyclebar-dunwoody&amp_device_id=kh_rxE7Htu-9fp39N3OiTU")
+    return driver
