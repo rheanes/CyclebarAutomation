8b1f78fb16e49cd57e65b66c1c0344863ad32603
Robert Eanes <95715277+rheanes@users.noreply.github.com>
2022-08-06 01:01:52 +0000

    moved login logic to login function

--- main.py
+++ main.py
@@ -17,15 +17,24 @@ def get_driver():
     driver.get("https://members.cyclebar.com/auth/login?location_id=cyclebar-dunwoody")
     return driver
 
+def login(driver):
+  time.sleep(2)
+  email = driver.find_element(by="id", value="form-input-email")
+  time.sleep(3)
+  password = driver.find_element(by="id", value="form-input-password")
+  email.send_keys("rhilleanes@gmail.com")
+  password.send_keys("seleniumAutomation1")
+  password.send_keys(Keys.RETURN)
+
 def main():
   driver = get_driver()
-  time.sleep(3)
-  driver.find_element(by="id", value="form-input-email").send_keys("rhilleanes@gmail.com")
-  time.sleep(3)
-  driver.find_element(by="id", value="form-input-password").send_keys("seleniumAutomation1" + Keys.RETURN)
+  login(driver)
+  
   time.sleep(3)
   if(driver.current_url == 'https://members.cyclebar.com/covid-waiver/cyclebar-dunwoody'):
     driver.find_element(by='xpath', value='/html/body/div[2]/div[2]/div/button').click()
+  time.sleep(6)
+  driver.find_element(by='xpath', value='/html/body/div[2]/div[2]/div[2]/div[4]/div/div[1]/small/a').click()
   time.sleep(20)
 
 main()
