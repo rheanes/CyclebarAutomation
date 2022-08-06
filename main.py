16dc661549fff3eb8752ae462533810e1b2245e2
Robert Eanes <95715277+rheanes@users.noreply.github.com>
2022-08-06 00:47:59 +0000

    Added log in step to browser

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from datetime import datetime as dt
+
 def get_driver():
     options = webdriver.ChromeOptions()
     options.add_argument("disable-infobars")
@@ -8,5 +14,18 @@ def get_driver():
     options.add_argument("disable-blink-features=AutomationControlled")
 
     driver = webdriver.Chrome(options=options)
-    driver.get("https://members.cyclebar.com/auth/login?location_id=cyclebar-dunwoody&amp_device_id=kh_rxE7Htu-9fp39N3OiTU")
+    driver.get("https://members.cyclebar.com/auth/login?location_id=cyclebar-dunwoody")
     return driver
+
+def main():
+  driver = get_driver()
+  time.sleep(3)
+  driver.find_element(by="id", value="form-input-email").send_keys("rhilleanes@gmail.com")
+  time.sleep(3)
+  driver.find_element(by="id", value="form-input-password").send_keys("seleniumAutomation1" + Keys.RETURN)
+  time.sleep(3)
+  if(driver.current_url == 'https://members.cyclebar.com/covid-waiver/cyclebar-dunwoody'):
+    driver.find_element(by='xpath', value='/html/body/div[2]/div[2]/div/button').click()
+  time.sleep(20)
+
+main()
